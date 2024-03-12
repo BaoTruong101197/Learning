e7092378c6d70ed2e32e676838463215021e4fdb
BaoTruong101197 <BaoTruong101197@gmail.com>
2024-03-12 21:03:33 +0700

    add smart pointer folder

---
+++ smartpointer/build/CMakeFiles/shared_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_ptr.dir/main.cpp.o"
+  "CMakeFiles/shared_ptr.dir/main.cpp.o.d"
+  "shared_ptr"
+  "shared_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
