27488a73fa6619970e50f86c949b9f1f798c9855
BaoTruong101197 <BaoTruong101197@gmail.com>
2024-03-24 12:03:37 +0700

    fix bug

--- concept_custom/main.cpp
+++ concept_custom/main.cpp
@@ -1,43 +1,8 @@
 #include <iostream>
-#include "WeakPtr.h"
 #include <memory>
 
 int main()
 {      
-    WeakPtr<int> ptr;
-    {
-        SharedPtr<int> ptr1 = SharedPtr<int>(new int(5));
-        ptr = ptr1;
-        SharedPtr<int> ptr2 = ptr1;
 
-        std::cout << "count of ptr: " << ptr.use_count() << std::endl;
-        std::cout << "expired of ptr: " << ptr.expired() << std::endl;
-        {
-            SharedPtr<int> temp = ptr.lock();
-        }
-        std::cout << "out scope of temp" << std::endl;
-
-        if (ptr.lock() == nullptr)
-        {
-            std::cout << "ptr is nullptr" << std::endl;
-        }
-        else 
-        {
-            std::cout << *ptr.lock() << std::endl;
-            std::cout << "count of ptr: " << ptr.use_count() << std::endl;
-        }
-    }
-    std::cout << "out scope of shared ptr" << std::endl;
-    if (ptr.lock() == nullptr)
-    {
-        std::cout << "ptr is nullptr" << std::endl;
-    }
-    else 
-    {
-        std::cout << *ptr.lock() << std::endl;
-        std::cout << "count of ptr: " << ptr.use_count() << std::endl;
-    }
-    std::cout << "count of ptr: " << ptr.use_count() << std::endl;
-    std::cout << "expired of ptr: " << ptr.expired() << std::endl;
     return 0;
 }
